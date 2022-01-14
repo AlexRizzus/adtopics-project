3cad97d7b7303d080794005f374e9dd52854875a
AlexRizzus <alex.rizzo1998@gmail.com>
2022-01-14 15:46:11 +0100

    implemented verify function

---
+++ aggregation.py
@@ -0,0 +1,49 @@
+from asyncio.windows_events import NULL
+import hashlib as hb
+from datetime import datetime
+
+g1,g2,g3 = 0
+pkO=0
+class Signature:
+    def __init__(self, t, B):
+        self.t = t
+        self.B = B
+
+class Att_Token:
+    def __init__(self, H, cl, vl, t, signo) -> None:
+        self.H = H
+        self.cl = cl
+        self.vl = vl
+        self.t = t
+        self.signo = signo
+
+def or_vector(H):
+    if H.count() == 1:
+        return H[0]
+    else:
+        return H[H.count()-1] | or_vector(H.pop())
+
+
+def Verify(apk, S, msg, sign):
+    apkm = apk / (multiply_vect(S) * multiply_vect(extract_public_keys(sign.B)))
+    if bilinear_map(sign.t, g2) == bilinear_map(hb.sha3_256(msg),apkm) * multiply_bilinear_maps(sign.B):
+        return sign.B
+    else:
+        return NULL
+
+
+def VerifyChallenge(token):
+    print("verification of the challenge started")
+    hg = hb.sha256(or_vector(token.H))
+    if (datetime.now() > datetime.strptime(token.t,"%H:%M:%S")):
+         print("invalid time for the request")
+         return 0
+    else:
+         if not (Verify(pkO,[],or_vector(list([hg,token.cl,token.vl,token.t])),token.signo)):
+              print("invalid signature for the request")
+              return 0
+    return 1
+
+
+
+
